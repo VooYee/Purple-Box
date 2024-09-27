68030990f7979f5b5b3e3290e3daae089dcf4d93
AtqiyaHaydar <ashford1524@gmail.com>
2024-09-27 09:45:38 +0700

    feat: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="font-agrandir">
-      Unlock the power of AI with Purple-Box, the multichannel chatbot designed for e-commerce growth.
+      
     </div>
   );
 }
