1cde0e56ccf0489118196c834592187daad8e923
AtqiyaHaydar <ashford1524@gmail.com>
2024-09-28 21:53:11 +0700

    feat: add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import RobotPad from "@/public/home/RobotPad.png"
 import EllipseGradient from "@/public/EllipseGradient.png"
+import Showcase from "@/public/home/Showcase.png"
+import LeftInfo from "@/public/home/LeftInfo.png"
+import RightInfo from "@/public/home/RightInfo.png"
 
 export default function Home() {
   return (
-    <div className="wrapper flex flex-col items-center gap-y-4">
+    <div className="wrapper flex flex-col items-center gap-y-16">
       
       {/* HERO */}
       <div className="mt-[80px] w-full text-center h-[500px] flex flex-col items-center justify-center gap-y-4 relative">
@@ -13,15 +16,43 @@ export default function Home() {
         <Image 
           src={RobotPad}
           alt="RobotPad"
-          width={450}
-          height={450}
+          width={500}
+          height={500}
         />
         <Image 
           src={EllipseGradient}
           alt="Ellipse Gradient"
           className="absolute -z-20"
         />
+        <div className="w-full max-w-md rounded-full background-gradient shadow-royal-purple h-[4px] my-4" />
       </div>
+
+      {/* SHOWCASE */}
+      <div className="w-full text-center flex flex-col items-center justify-center gap-y-12 relative">
+        <p className="text-xl text-center">AI-powered multichannel chatbot designed for e-commerce <br/> businesses to enhance customer acquisition and provide seamless 24/7 support.</p>
+        <Image 
+          src={Showcase}
+          alt="Product Showcase"
+          width={450}
+          height={450}
+        />
+        <Image 
+          src={EllipseGradient}
+          alt="Ellipse Gradient"
+          className="absolute left-[500px] -z-20"
+        />
+        <Image 
+          src={RightInfo}
+          alt="Left Info"
+          className="absolute translate-x-[250px] w-[225px] translate-y-[-25px]"
+        />
+      </div>
+
+      {/* TRANSOFRM ECOMMERCE */}
+      <div className="w-full text-center flex flex-col items-center justify-center gap-y-12 relative">
+        <h5 className="font-gotham text-4xl font-bold">Transform <span className="text-gradient">Your E-Commerce</span> With Purple Box</h5>
+      </div>
+
     </div>
   );
 }
