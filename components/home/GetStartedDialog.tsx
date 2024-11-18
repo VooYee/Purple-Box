6eb2d93a28de2b41d0a06021a907bac1c0db9e4b
AtqiyaHaydar <ashford1524@gmail.com>
2024-11-19 00:32:09 +0700

    feat: add dialog

--- components/home/GetStartedDialog.tsx
+++ components/home/GetStartedDialog.tsx
@@ -12,13 +12,11 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
