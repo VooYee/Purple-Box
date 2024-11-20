30786a9459243b52ea9982908881cd2439d499de
AtqiyaHaydar <ashford1524@gmail.com>
2024-11-20 11:08:06 +0700

    feat: ini client page

---
+++ app/admin/client-message/page.tsx
@@ -0,0 +1,25 @@
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="bg-[#1a0730] wrapper flex flex-col items-center mt-[80px]">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
